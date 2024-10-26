854247ce22495448ed6baba6a402f9091815afb8
Markus Heid <heid.markus@web.de>
2024-10-26 23:37:23 +0200

    Updated: * Print out of dependencies * Direct call of pandoc to create pdf out of md

--- SolutionMapPDFPrinter/Program.cs
+++ SolutionMapPDFPrinter/Program.cs
@@ -1,12 +1,11 @@
-﻿using EnvDTE;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
+using EnvDTE;
 
 namespace SolutionMapPDFPrinter
 {
@@ -27,11 +26,8 @@ namespace SolutionMapPDFPrinter
                 return;
             }
 
-            
-
-            // Access DTE object as before
+            // Access DTE object
             var dte2 = (EnvDTE80.DTE2)Marshal.GetActiveObject("VisualStudio.DTE.17.0");
-            //var dte2 = (EnvDTE80.DTE2)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE.17.0");
             dte2.Solution.Open(solutionPath);
             var solution = dte2.Solution;
             var solutionName = Path.GetFileNameWithoutExtension(solution.FullName);
@@ -43,15 +39,27 @@ namespace SolutionMapPDFPrinter
                 markdownContent += $"## {project.Name}\n\n";
                 markdownContent += "### Dependencies\n\n";
                 var packages = GetPackages(project);
-                foreach (var package in packages)
+                if (packages.Count == 0)
                 {
-                    markdownContent += $"- {package.Name} v{package.Version}\n";
+                    markdownContent += "- No dependencies found\n";
+                }
+                else
+                {
+                    foreach (var package in packages)
+                    {
+                        markdownContent += $"- {package.Name} v{package.Version}\n";
+                    }
                 }
                 markdownContent += "\n";
                 // Process each project item recursively as before
                 ProcessProjectItems(project.ProjectItems, ref markdownContent, solutionDir);
             }
-            File.WriteAllText($"{solutionDir}/{solutionName}.md", markdownContent);
+            var markdownFilePath = $"{solutionDir}/{solutionName}.md";
+            File.WriteAllText(markdownFilePath, markdownContent);
+
+            // Call Pandoc to convert Markdown to PDF
+            string pdfFilePath = $"{solutionDir}/{solutionName}.pdf";
+            CallPandoc(markdownFilePath, pdfFilePath);
         }
 
         static void ProcessProjectItems(ProjectItems items, ref string markdownContent, string rootPath)
@@ -62,7 +70,7 @@ namespace SolutionMapPDFPrinter
                 {
                     string filePath = item.FileNames[1];
                     markdownContent += $"### {filePath.Replace(rootPath, "").Replace("\\", "/")}\n\n";
-                    markdownContent += "```csharp\n" + File.ReadAllText(filePath) + "\n```\n\n";
+                    markdownContent += "```csharp\n" + File.ReadAllText(filePath) + "\n````\n\n";
                 }
                 if (item.ProjectItems != null && item.ProjectItems.Count > 0)
                 {
@@ -70,7 +78,6 @@ namespace SolutionMapPDFPrinter
                 }
             }
         }
-
 
         static List<(string Name, string Version)> GetPackages(Project project)
         {
@@ -80,7 +87,7 @@ namespace SolutionMapPDFPrinter
             {
                 var doc = XDocument.Load(projectFilePath);
                 XNamespace msbuild = "http://schemas.microsoft.com/developer/msbuild/2003";
-                foreach (var packageReference in doc.Descendants(msbuild + "PackageReference"))
+                foreach (var packageReference in doc.Descendants().Where(e => e.Name.LocalName == "PackageReference"))
                 {
                     string name = packageReference.Attribute("Include")?.Value;
                     string version = packageReference.Attribute("Version")?.Value;
@@ -93,5 +100,40 @@ namespace SolutionMapPDFPrinter
             return packageList;
         }
 
+        static void CallPandoc(string markdownFilePath, string pdfFilePath)
+        {
+            try
+            {
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = "pandoc",
+                    Arguments = $"\"{markdownFilePath}\" -o \"{pdfFilePath}\" --pdf-engine=xelatex",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = System.Diagnostics.Process.Start(processInfo))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    if (process.ExitCode != 0)
+                    {
+                        Console.WriteLine($"Pandoc Error: {error}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("PDF generated successfully.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while calling Pandoc: {ex.Message}");
+            }
+        }
     }
 }
